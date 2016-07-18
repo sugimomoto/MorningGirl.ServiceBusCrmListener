f0c88695f676397ac6406d40f097a26df85aee9b
sugimomoto <sugimomoto@gmail.com>
2016-07-18 20:00:50 +0900

    Start

---
+++ MyProject.ServiceBusCrmListener/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.ServiceBus.Messaging;
+using Microsoft.Xrm.Sdk;
+
+namespace MyProject.ServiceBusCrmListener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SAS ConnectionStringを入力してください");
+            var connectionString = Console.ReadLine();
+            
+            var client = QueueClient.CreateFromConnectionString(connectionString);
+            
+            client.OnMessage(message =>
+                {
+                    // PluginのContext情報取得
+                    var context = message.GetBody<RemoteExecutionContext>();
+                    Console.WriteLine(context.PrimaryEntityId);
+                }
+                );
+            
+            Console.WriteLine("処理を終了します。Enterを押下してください。");
+            Console.ReadKey();
+        }
+    }
+}
