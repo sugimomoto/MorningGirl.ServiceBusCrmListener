0e10aba4bd1adb1124f917f94fa8c0c0a121027b
sugimomoto <sugimomoto@gmail.com>
2016-08-07 21:46:16 +0900

    微調整

---
+++ MyProject.ServiceBusCrmListener/CrmPluginDataModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorningGirl.ServiceBusCrmListener
+{
+    public class CrmPluginDataModel
+    {
+        public int ContextId { get; set; }
+        public string EntityName { get; set; }
+        public string UserName { get; set; }
+        public string Message { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
--- MyProject.ServiceBusCrmListener/Program.cs
+++ MyProject.ServiceBusCrmListener/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using Microsoft.ServiceBus.Messaging;
 using Microsoft.Xrm.Sdk;
+using System.Threading;
 
-namespace MyProject.ServiceBusCrmListener
+namespace MorningGirl.ServiceBusCrmListener
 {
     class Program
     {
@@ -11,18 +12,26 @@ namespace MyProject.ServiceBusCrmListener
             Console.WriteLine("SAS ConnectionStringを入力してください");
             var connectionString = Console.ReadLine();
             
-            var client = QueueClient.CreateFromConnectionString(connectionString);
-            
-            client.OnMessage(message =>
-                {
-                    // PluginのContext情報取得
-                    var context = message.GetBody<RemoteExecutionContext>();
-                    Console.WriteLine(context.PrimaryEntityId);
-                }
-                );
-            
-            Console.WriteLine("処理を終了します。Enterを押下してください。");
-            Console.ReadKey();
+            // 1秒ごとにメッセージを取得
+            while(true){
+
+                var counter = 0;
+
+                var client = QueueClient.CreateFromConnectionString(connectionString);
+
+                client.OnMessage(message =>
+                    {
+                        // PluginのContext情報取得
+                        var context = message.GetBody<RemoteExecutionContext>();
+                        var input = context.InputParameters["Target"] as Entity;
+                        Console.WriteLine(string.Format("count : {0} , EntityId : {1} , EntityName : {2}, RecordName : {3}",counter,context.PrimaryEntityId,context.PrimaryEntityName,input["name"].ToString()));
+
+                        counter++;
+                    }
+                    );
+                   
+                Thread.Sleep(1000);
+            }
         }
     }
 }
